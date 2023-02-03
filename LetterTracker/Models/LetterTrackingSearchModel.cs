b89741a96f9aa1456c9ea1b237fbe38ab2a75452
Kyle Skinner <kyleskin@gmail.com>
2023-02-03 13:56:19 -0700

    finished switching to mudblazor

--- LetterTracker/Models/LetterTrackingSearchModel.cs
+++ LetterTracker/Models/LetterTrackingSearchModel.cs
@@ -11,21 +11,54 @@ namespace LetterTracker.Models
         public string? EocId { get; set; }
         //[Required]
         public string? MemberId { get; set; }
-        public string? AgentUserId  { get; set; }
-        public ManualLetterReasonEnum ReasonForManualLetter { get; set; } = ManualLetterReasonEnum.VerbalNotificationUnsuccessful;
-        public DateOnly? ManualMailDate { get; set; }
-        public TimeOnly? ManualMailTime { get; set; }
-        public TimeZoneEnum TimeZone { get; set; } = TimeZoneEnum.Mst;
-        public LetterTypeEnum LetterType { get; set; } = LetterTypeEnum.FinalNotification;
+        public string? AgentUserId { get; set; }
+        public ManualLetterReasonEnum ReasonForManualLetter { get; set; }
+        public DateTime? ManualMailDate { get; set; }
+        public TimeSpan? ManualMailTime { get; set; }
+        public TimeZoneEnum TimeZone { get; set; }
+        public LetterTypeEnum LetterType { get; set; }
         public ManualProcessDayEnum ManualProcessDay { get; set; }
+        [Required]
+        public ManualProcessEnum? ManualProcessUsed
+        {
+            get
+            {
+                ManualProcessEnum? _manualProcess;
+                if (ManualProcessDay == ManualProcessDayEnum.BusinessDay)
+                {
+                    _manualProcess = Enum.TryParse(BusinessDayManualProcessRadioOption, out ManualProcessEnum manualProcess)
+                        ? manualProcess
+                        : null;
+                }
+                else if (ManualProcessDay == ManualProcessDayEnum.Saturday)
+                {
+                    _manualProcess = Enum.TryParse(SaturdayManualProcessRadioOption, out ManualProcessEnum manualProcess)
+                        ? manualProcess
+                        : null;
+                }
+                else if (ManualProcessDay == ManualProcessDayEnum.Sunday)
+                {
+                    _manualProcess = Enum.TryParse(SundayManualProcessRadioOption, out ManualProcessEnum manualProcess)
+                        ? manualProcess
+                        : null;
+                }
+                else
+                {
+                    _manualProcess = Enum.TryParse(HolidayManualProcessRadioOption, out ManualProcessEnum manualProcess)
+                        ? manualProcess
+                        : null;
+                }
+                return _manualProcess;
+            }
+        }
         public ManualProcessEnum? BusinessDayManualProcessUsed { get; set; }
-        public ManualProcessEnum? BusinessDayManualProcessCached { get; set; }
+        public string? BusinessDayManualProcessRadioOption { get; set; }
         public ManualProcessEnum? SaturdayManualProcessUsed { get; set; }
-        public ManualProcessEnum? SaturdayManualProcessCached { get; set; }
+        public string? SaturdayManualProcessRadioOption { get; set; }
         public ManualProcessEnum? SundayManualProcessUsed { get; set; }
-        public ManualProcessEnum? SundayManualProcessCached { get; set; }
+        public string? SundayManualProcessRadioOption { get; set; }
         public ManualProcessEnum? HolidayManualProcessUsed { get; set; }
-        public ManualProcessEnum? HolidayManualProcessCached { get; set; }
+        public string? HolidayManualProcessRadioOption { get; set; }
         public string? Comments { get; set; }
     }
 }
